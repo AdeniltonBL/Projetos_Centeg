5da65e0dd5cf982d799377a8e3dc49f3a2fadb8d
AdeniltonBL <rononoasasuke@gmail.com>
2026-03-24 20:47:38 -0300

    sistemas

---
+++ sistemas/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim sistemas (3) as String
+        sistemas(0) = "Linux"
+        sistemas(1) = "Windows"
+        sistemas(2) = "IOS"
+
+        console.writeline(sistemas(0))
+        console.writeline(sistemas(1))
+        console.writeline(sistemas(2))
+    End Sub
+End Module
