30a7d57dd284ee6c77a5ca5c171f1ae585034742
AdeniltonBL <rononoasasuke@gmail.com>
2026-03-24 20:18:47 -0300

    vetores

--- vetores/Program.vb
+++ vetores/Program.vb
@@ -7,7 +7,7 @@ Module Program
         nomecompleto(0) = "Adenilton"
         nomecompleto(1) = "Brito"
 
-        console.writeline(nomecompleto(0) & (1))
+        console.writeline(nomecompleto(0))
         console.writeline(nomecompleto(1))
 
     End Sub
