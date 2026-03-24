5115b5176affae22d497a512455d52b5e32009dd
AdeniltonBL <rononoasasuke@gmail.com>
2026-03-24 20:17:11 -0300

    vetores

---
+++ vetores/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim nomecompleto(2) as String
+        
+        nomecompleto(0) = "Adenilton"
+        nomecompleto(1) = "Brito"
+
+        console.writeline(nomecompleto(0) & (1))
+        console.writeline(nomecompleto(1))
+
+    End Sub
+End Module
