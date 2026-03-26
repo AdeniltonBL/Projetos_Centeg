134728676bf9291cd3cd10b14dc149cb9de5a9c9
AdeniltonBL <rononoasasuke@gmail.com>
2026-03-26 20:59:43 -0300

    arrays

---
+++ arrays/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim tabuada as integer
+
+        for tabuada = 1 to 10
+                console.writeline("5 * "& tabuada & " = " & tabuada * 5)
+        next
+        
+
+    End Sub
+End Module
